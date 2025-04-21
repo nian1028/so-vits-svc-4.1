31da9f7800a6e1fbf6bffc770f4e42c8f375d850
nian1028 <144693948+nian1028@users.noreply.github.com>
2025-04-21 13:31:31 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -238,7 +238,7 @@ def plot_spectrogram_to_numpy(spectrogram):
   plt.tight_layout()
 
   fig.canvas.draw()
-  data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+  data = np.fromstring(fig.canvas.buffer_rgba(), dtype=np.uint8)
   data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
   plt.close()
   return data
