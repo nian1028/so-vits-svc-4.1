3bc8e1ad4a891070915e70f81f4f5e078d612d83
nian1028 <144693948+nian1028@users.noreply.github.com>
2025-04-21 13:41:23 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -60,8 +60,8 @@ def plot_data_to_numpy(x, y):
     plt.tight_layout()
 
     fig.canvas.draw()
-    data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    data = np.fromstring(fig.canvas.buffer_rgba(), dtype=np.uint8)
+    data = data.reshape(fig.canvas.get_width_height()[::-1] + (4,))
     plt.close()
     return data
 
